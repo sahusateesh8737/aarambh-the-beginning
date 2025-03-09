97d255136d444bb4ef41f3f98ee8d24320410472
sahusateesh8737 <sahusateesh8737@gmail.com>
2025-03-09 11:53:19 +0530

    Update port configuration for deployment

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import User from './models/User.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
@@ -73,7 +74,8 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
